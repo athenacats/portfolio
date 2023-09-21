7c06325b9611e7e11adb4172d6b70851a192bd76
athenacats <124017569+athenacats@users.noreply.github.com>
2023-09-21 20:09:45 +0300

    Routing for projects page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/pages/about/about.component';
 import { SkillsComponent } from './components/pages/skills/skills.component';
+import { ProjectsComponent } from './components/pages/projects/projects.component';
 
 const routes: Routes = [
   { path: '', component: AboutComponent },
   { path: 'skills', component: SkillsComponent },
+  { path: 'projects', component: ProjectsComponent },
 ];
 
 @NgModule({
