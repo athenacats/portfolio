3d4c7beddeccd4c7e4e0c90ab5ae93549c7276a7
athenacats <124017569+athenacats@users.noreply.github.com>
2023-09-22 13:58:15 +0300

    Add code for button functionality

--- frontend/src/app/components/pages/projects/projects.component.ts
+++ frontend/src/app/components/pages/projects/projects.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'],
 })
 export class ProjectsComponent {
+  somaLive(): void {
+    window.open('https://soma-production.up.railway.app/');
+  }
 
+  somaCode(): void {
+    window.open('https://github.com/athenacats/soma');
+  }
 }
