6bb88c1edb8a7a82d5237242c3296d059eb600aa
athenacats <124017569+athenacats@users.noreply.github.com>
2023-09-22 16:53:43 +0300

    Add contact info

--- frontend/src/app/components/pages/contact/contact.component.ts
+++ frontend/src/app/components/pages/contact/contact.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
+  mailTo(): void {
+    window.open('mailto:chenalonya@gmail.com');
+  }
 
+  linkedIn(): void {
+    window.open('https://www.linkedin.com/in/esther-lonyangapuo/');
+  }
+
+  github(): void {
+    window.open('https://github.com/athenacats/');
+  }
 }
